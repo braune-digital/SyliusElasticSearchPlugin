1ed3c136247eef2c70dee59387348c3e73a1a180
Henrik Braune <hb@braune-digital.com>
2018-05-18 10:51:10 +0200

    Added serialization groups

--- src/Controller/ImageView.php
+++ src/Controller/ImageView.php
@@ -4,15 +4,26 @@ declare(strict_types=1);
 
 namespace Sylius\ElasticSearchPlugin\Controller;
 
+use JMS\Serializer\Annotation as Serializer;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
+use JMS\Serializer\Annotation\Groups;
+
+/**
+ * @ExclusionPolicy("all")
+ */
 class ImageView implements ViewInterface
 {
     /**
      * @var string
+     * @Groups({"autocomplete", "search"})
      */
     public $code;
 
     /**
      * @var string
+     * @Expose
+     * @Groups({"autocomplete", "search"})
      */
     public $path;
 }
