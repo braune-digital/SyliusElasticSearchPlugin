e4703ddc5818b1ed471f29a12a52babe772b6209
Henrik Braune <hb@braune-digital.com>
2018-05-22 16:37:29 +0200

    Allow and and or options in term filter

--- src/Filter/Widget/SingleTermChoice.php
+++ src/Filter/Widget/SingleTermChoice.php
@@ -9,20 +9,20 @@ use ONGR\ElasticsearchDSL\Query\Joining\NestedQuery;
 use ONGR\ElasticsearchDSL\Query\TermLevel\TermQuery;
 use ONGR\ElasticsearchDSL\Search;
 use ONGR\FilterManagerBundle\Filter\FilterState;
-use ONGR\FilterManagerBundle\Filter\Widget\Choice\SingleTermChoice;
+use ONGR\FilterManagerBundle\Filter\Widget\Choice\SingleTermChoice as BaseSingleTermChoice;
 use ONGR\FilterManagerBundle\Search\SearchRequest;
 
 /**
  * This class provides single terms choice.
  */
-class SingleNestedTermChoice extends SingleTermChoice
+class SingleTermChoice extends BaseSingleTermChoice
 {
+
     /**
      * {@inheritdoc}
      */
     public function modifySearch(Search $search, FilterState $state = null, SearchRequest $request = null)
     {
-        [$path, $field] = explode('>', $this->getDocumentField());
         $boolType = BoolQuery::MUST;
 
         if ($this->hasOption('operator') && in_array($this->getOption('operator'), ['and', 'or'])) {
@@ -32,15 +32,8 @@ class SingleNestedTermChoice extends SingleTermChoice
                     break;
             }
         }
-
         if ($state && $state->isActive()) {
-            $search->addPostFilter(
-                new NestedQuery(
-                    $path,
-                    new TermQuery($field, $state->getValue())
-                ),
-                $boolType
-            );
+            $search->addPostFilter(new TermQuery($this->getDocumentField(), $state->getValue()), $boolType);
         }
     }
 }
